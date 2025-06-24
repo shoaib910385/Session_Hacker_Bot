a0d71696cf6e540e1713a8ae064a298db78ebc88
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-06-24 23:03:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/BEASTUSERBOT/Session_Hacker_Bot")
+os.system("git clone https://github.com/shoaib910385/Session_Hacker_Bot")
 os.system("cd Session_Hacker_Bot && python bot.py")
